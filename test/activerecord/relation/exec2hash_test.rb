1aa16e3f17486597eb7786005b416431472cf4d9
HaiTo <thewordr2.ver.xes@gmail.com>
2017-07-19 10:29:14 +0900

    add null relation test. and after drop model records

--- test/activerecord/relation/exec2hash_test.rb
+++ test/activerecord/relation/exec2hash_test.rb
@@ -1,12 +1,27 @@
 require "test_helper"
 
 class Activerecord::Relation::Exec2hashTest < Minitest::Test
-  def test_it_does_something_useful
-    Post.create(content: 'text1')
-    Post.create(content: 'text2')
+  def after_teardown
+    Post.delete_all
+
+    super
+  end
+
+  def test_basic_pattern
+    post_1 = Post.create(content: 'text1')
+    post_2 = Post.create(content: 'text2')
 
     posts = Post.all.exec_to_hash
 
-    assert_equal(posts, [{'id' => 1, 'content' => 'text1'}, {'id' => 2, 'content' => 'text2'}])
+    assert_equal(posts, [{'id' => post_1.id, 'content' => 'text1'}, {'id' => post_2.id, 'content' => 'text2'}])
+  end
+
+  def test_basic_null_relation_pattern
+    Post.create(content: 'text1')
+    Post.create(content: 'text2')
+
+    posts = Post.where(id: 0).exec_to_hash
+
+    assert_equal(posts, [])
   end
 end

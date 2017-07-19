bb9dfd1d522499f2a206b2df868ced6c9ab39f30
HaiTo <thewordr2.ver.xes@gmail.com>
2017-07-19 10:20:44 +0900

    not use reopen classes

--- lib/activerecord/relation/exec2hash/null_relation.rb
+++ lib/activerecord/relation/exec2hash/null_relation.rb
@@ -1,7 +1,9 @@
-module ActiveRecord
+module Exec2Hash
   module NullRelation
     def exec_to_hash
       []
     end
   end
 end
+
+ActiveRecord::NullRelation.send(:include, Exec2Hash::NullRelation)
--- lib/activerecord/relation/exec2hash/relation.rb
+++ lib/activerecord/relation/exec2hash/relation.rb
@@ -1,7 +1,9 @@
-module ActiveRecord
-  class Relation
+module Exec2Hash
+  module Relation
     def exec_to_hash
       connection.exec_query(to_sql).to_hash
     end
   end
 end
+
+ActiveRecord::Relation.send(:include, Exec2Hash::Relation)
